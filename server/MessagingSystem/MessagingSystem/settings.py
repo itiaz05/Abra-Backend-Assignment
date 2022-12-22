561efe4cf83c640f10bcf0c1948312f3dc68d749
itiaz05 <itiaz05@gmail.com>
2022-12-22 15:47:30 +0200

    Update settings.py

--- server/MessagingSystem/MessagingSystem/settings.py
+++ server/MessagingSystem/MessagingSystem/settings.py
@@ -149,7 +149,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]    
 
-LOGIN_URL = "accounts/login/"
+LOGIN_URL = "accounts:login"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
